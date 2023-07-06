ccf9e71dc6d30107f25f8a2558f5d81aaf98f3f8
imamovicazra <azra.imamovic998@gmail.com>
2023-07-06 14:43:27 +0200

    Added AutoMapper

---
+++ IdentityServer/Identity.API/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Identity.Model.DTOs.Requests;
+using Identity.Model.DTOs.Responses;
+using Identity.Model.Entities;
+
+namespace Identity.API.AutoMapper
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ApplicationUser, ApplicationUserRequest>()
+                .ForMember(dest => dest.Email, opts => opts.MapFrom(src => src.UserName))
+                .ReverseMap();
+
+            CreateMap<ApplicationUser, ApplicationUserResponse>()
+                .ForMember(dest => dest.IsEmailConfirmed, opts => opts.MapFrom(src => src.EmailConfirmed))
+                .ForMember(dest => dest.UserId, opts => opts.MapFrom(src => src.Id))
+                .ReverseMap();
+
+            CreateMap<ApplicationUser, UserResponse>()
+                .ForMember(dest => dest.UserId, opts => opts.MapFrom(src => src.Id))
+                .ForMember(dest => dest.IsEmailConfirmed, opts => opts.MapFrom(src => src.EmailConfirmed))
+                .ReverseMap();
+        }
+    }
+}
---
+++ IdentityServer/Identity.Model/DTOs/Responses/ApplicationUserResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Model.DTOs.Responses
+{
+    public class ApplicationUserResponse
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public ApplicationUserResponse(string userId, string firstName, string lastName, string email, bool isEmailConfirmed)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            IsEmailConfirmed = isEmailConfirmed;
+        }
+        public ApplicationUserResponse() { }
+    }
+}
